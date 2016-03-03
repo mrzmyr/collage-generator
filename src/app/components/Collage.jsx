fe6bbcd145c8ceac70567de4088a6f3e777360cf
mrzmyr <m0ritz.m3yer@gmail.com>
2016-03-03 15:14:11 +0100

    add shuffle button

--- src/app/components/Collage.jsx
+++ src/app/components/Collage.jsx
@@ -88,13 +88,13 @@ class Collage extends React.Component {
         {(() => {
           if(this.state.generated) {
             return (
-              <a className={ !this.props.images.length ? 'take-snapshot take-snapshot--disabled' : 'take-snapshot' } href="javascript:;">
+              <a className={ !this.props.images.length ? 'take-snapshot link--disabled' : 'take-snapshot' } href="javascript:;">
                 <svg width="24" height="24" viewBox="0 0 24 24"><path fill="#FFF" d="M16 11h5l-9 10-9-10h5v-11h8v11zm1 11h-10v2h10v-2z"/></svg>
               </a>
             )
           } else {
             return (
-              <a onClick={this.onGenerate.bind(this)} className={ !this.props.images.length ? 'take-snapshot take-snapshot--disabled' : 'take-snapshot' } href="javascript:;">
+              <a onClick={this.onGenerate.bind(this)} className={ !this.props.images.length ? 'take-snapshot link--disabled' : 'take-snapshot' } href="javascript:;">
                 <svg width="24" height="24" viewBox="0 0 24 24"><path fill="#FFF" d="M24 14v-4h-3.23c-.229-1.003-.624-1.94-1.156-2.785l2.286-2.286-2.83-2.829-2.286 2.286c-.845-.532-1.781-.928-2.784-1.156v-3.23h-4v3.23c-1.003.228-1.94.625-2.785 1.157l-2.286-2.286-2.829 2.828 2.287 2.287c-.533.845-.928 1.781-1.157 2.784h-3.23v4h3.23c.229 1.003.624 1.939 1.156 2.784l-2.286 2.287 2.829 2.829 2.286-2.286c.845.531 1.782.928 2.785 1.156v3.23h4v-3.23c1.003-.228 1.939-.624 2.784-1.156l2.286 2.286 2.828-2.829-2.285-2.286c.532-.845.928-1.782 1.156-2.785h3.231zm-12 2c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z"/></svg>
               </a>
             )
